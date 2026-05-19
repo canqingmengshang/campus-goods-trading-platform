04f81edb42a8a65d3fdc4b417e76469d89fb4943
liuguangxing <2842055162@qq.com>
2026-05-19 10:32:35 +0800

    可运行版本

---
+++ src/main/java/org/example/campusgoodstradingplatform/CampusGoodsTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.campusgoodstradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGoodsTradingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusGoodsTradingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/campusgoodstradingplatform/CampusGoodsTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campusgoodstradingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGoodsTradingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
